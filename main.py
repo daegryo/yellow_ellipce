35f58603d0b141aabdef7b3dd3e6f68df9cb24e9
daryaalbertovna <yegorova082@yandex.ru>
2023-11-24 00:33:50 +0300

    yellow_ellipses

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+import random
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from des import Ui_MainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Ellipse(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            # Начинаем процесс рисования
+            qp.begin(self)
+            qp.setBrush(QColor(255, 255, 0))
+            r = random.randint(20, 100)
+            x, y = random.randint(0, 800), random.randint(0, 600)
+            qp.drawEllipse(int(x - r / 2), int(y - r / 2), r, r)
+            # Завершаем рисование
+            qp.end()
+            self.do_paint = False
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Ellipse()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
